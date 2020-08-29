84db35ce8fcf2c6ac4da6e70683fa83573d3336c
vasu2001 <vasudhruv1920@gmail.com>
2020-08-29 22:28:44 +0530

    admob unitId

--- src/utils/admob.tsx
+++ src/utils/admob.tsx
@@ -1,4 +1,4 @@
 import {TestIds} from '@react-native-firebase/admob';
 
-export const bannerUnitId =
-  'ca-app-pub-2141642156479856/1034311734' || TestIds.BANNER;
+export const bannerUnitId = 'ca-app-pub-2141642156479856/4426761836';
+//  TestIds.BANNER;
